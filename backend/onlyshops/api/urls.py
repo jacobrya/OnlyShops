fa6abe114b221e6c05498e65936d75fff37900b3
zhanibek <z_darkhanuly@kbtu.kz>
2025-04-19 17:41:25 +0500

    added register api

--- backend/onlyshops/api/urls.py
+++ backend/onlyshops/api/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from api import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -6,5 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/register/', views.RegisterView.as_view(), name='register'),
+
 
 ]
--- backend/onlyshops/api/views.py
+++ backend/onlyshops/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, password=password)
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
