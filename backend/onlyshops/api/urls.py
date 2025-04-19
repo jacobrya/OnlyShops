136ce50b680b27187359cfab34f55b4120b63837
zhanibek <z_darkhanuly@kbtu.kz>
2025-04-19 17:31:10 +0500

    added jwt

---
+++ backend/onlyshops/api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from api import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
