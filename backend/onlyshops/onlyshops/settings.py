d0f5849227aca75bf5297175f89e69fc29b0b310
zhanibek <z_darkhanuly@kbtu.kz>
2025-04-19 16:59:55 +0500

    added corsheaders

--- backend/onlyshops/onlyshops/settings.py
+++ backend/onlyshops/onlyshops/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True  # For development only
+# For production, specify allowed origins:
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
