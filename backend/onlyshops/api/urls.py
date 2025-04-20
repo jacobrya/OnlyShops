bd9d5d04f34c8c1e5480940c0e56095460f8dd94
zhanibek <z_darkhanuly@kbtu.kz>
2025-04-20 18:17:44 +0500

    added models,views, serializers

--- backend/onlyshops/api/urls.py
+++ backend/onlyshops/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from api import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import *
 
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/register/', views.RegisterView.as_view(), name='register'),
+    path('products/', product_list_create),
+    path('products/<int:pk>/', product_detail),
+    path('categories/', CategoryListCreateView.as_view()),
+    path('categories/<int:pk>/', CategoryDetailView.as_view()),
 
 
 ]
