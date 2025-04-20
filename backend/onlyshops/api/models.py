8d562f22430038fab94827dff92502cab651ff41
Doni <laksling@gmail.com>
2025-04-20 20:01:55 +0500

    back register and login protected

--- backend/onlyshops/api/models.py
+++ backend/onlyshops/api/models.py
@@ -1,36 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('admin', 'Admin'),
+        ('seller', 'Seller'),
+    )
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')  # 'user', 'seller',
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.TextField(blank=True, null=True)
+    stock = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-    
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.TextField()
-    phone_number = models.CharField(max_length=15)
+        return self.title
 
-    def __str__(self):
-        return self.user.username
     
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=(
-        ('pending', 'Pending'),
-        ('shipped', 'Shipped'),
-        ('delivered', 'Delivered'),
-    ))
+# class Order(models.Model):
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     status = models.CharField(max_length=20, choices=(
+#         ('pending', 'Pending'),
+#         ('shipped', 'Shipped'),
+#         ('delivered', 'Delivered'),
+#     ))
